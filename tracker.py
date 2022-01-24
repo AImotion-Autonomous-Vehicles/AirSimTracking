16f988b23841c2b62dbc91da5e6ff107ebcd2a47
Lucas Correa de Assis <lucascorreadeassis@gmail.com>
2022-01-24 11:26:19 -0300

    Changed coordinates to be float, overloaded equal operator of coordinates

--- tracker.py
+++ tracker.py
@@ -2,15 +2,20 @@ from typing import List
 
 class Coordinates:
 
-    x: int
-    y: int
+    x: float
+    y: float
 
     def __str__(self) -> str:
         return f'({self.x}, {self.y})'
 
-    def __init__(self, x: int, y: int) -> None:
-        self.x = int(x)
-        self.y = int(y)
+    def __init__(self, x: float, y: float) -> None:
+        self.x = float(x)
+        self.y = float(y)
+
+    def __eq__(self, other: object) -> bool:
+        if self.x == other.x and self.y == other.y:
+            return True
+        return False
 
 class Tracker:
 
@@ -19,16 +24,13 @@ class Tracker:
 
     def __init__(self, initial_coordinates: Coordinates) -> None:
         self._initial_coordinates = initial_coordinates
-        self._coordinates = [Coordinates(0,0)]
+        self._coordinates = [Coordinates(0.0,0.0)]
 
     def update_coordinates(self, coordinates: Coordinates) -> None:
         coordinates.x -= self._initial_coordinates.x
         coordinates.y -= self._initial_coordinates.y
 
-        if coordinates.x == self._coordinates[len(self._coordinates)-1].x:
-            return
-
-        if coordinates.y == self._coordinates[len(self._coordinates)-1].y:
+        if coordinates == self._coordinates[len(self._coordinates)-1]:
             return
 
         self._coordinates.append(coordinates)
