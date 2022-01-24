bba08cad62692a8b6fb4a39921d50a6263116fee
Lucas Correa de Assis <lucascorreadeassis@gmail.com>
2022-01-24 11:41:15 -0300

    Improved abstraction of early return when updating coordinates

--- tracker.py
+++ tracker.py
@@ -30,10 +30,13 @@ class Tracker:
         coordinates.x -= self._initial_coordinates.x
         coordinates.y -= self._initial_coordinates.y
 
-        if coordinates == self._coordinates[len(self._coordinates)-1]:
+        if coordinates == self.__get_last_coordinates():
             return
 
         self._coordinates.append(coordinates)
+
+    def __get_last_coordinates(self):
+        return self._coordinates[len(self._coordinates)-1]
 
     def get_coordinates(self) -> List[Coordinates]:
         return self._coordinates
